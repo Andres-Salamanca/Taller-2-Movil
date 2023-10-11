5ef5a0aa6789573d1e2af170de532677c67dd5f0
ALEYI17 <aleelaleyi@gmail.com>
2023-10-11 17:48:44 -0500

    Termine bien lo del longpress

--- app/src/main/java/com/icm/taller2movil/OsmMap.kt
+++ app/src/main/java/com/icm/taller2movil/OsmMap.kt
@@ -59,20 +59,8 @@ class OsmMap : AppCompatActivity() {
         }
         cambioMapa()
 
-        val mapEventsOverlay = MapEventsOverlay(object : MapEventsReceiver {
-            override fun singleTapConfirmedHelper(p: GeoPoint?): Boolean {
-                return false
-            }
 
-            override fun longPressHelper(p: GeoPoint?): Boolean {
-                p?.let {
-                    longPressOnMap(it)
-                    return true
-                }
-                return false
-            }
-        })
-        binding.map.overlays.add(0, mapEventsOverlay)
+        binding.map.overlays.add(createOverlayEvent())
 
         binding.editTextText.setOnEditorActionListener { textView, actionId, keyEvent ->
             if (actionId == EditorInfo.IME_ACTION_DONE ||
@@ -142,24 +130,57 @@ class OsmMap : AppCompatActivity() {
         sensorManager.unregisterListener(sensorEventListener)
     }
 
-    private fun longPressOnMap(p: GeoPoint) {
-        longPressedMarker?.let { binding.map.overlays.remove(it) }
-        longPressedMarker = createMarker(p, "location", null, R.drawable.baseline_push_pin_24)
-        longPressedMarker?.let { binding.map.overlays.add(it) }
+    private fun createOverlayEvent():MapEventsOverlay{
+        val receiver = object : MapEventsReceiver {
+            override fun singleTapConfirmedHelper(p: GeoPoint?): Boolean {
+                return false
+            }
+
+
+            override fun longPressHelper(p: GeoPoint): Boolean {
+                longPressOnMap(p)
+                return true
+            }
+        }
+        return MapEventsOverlay(receiver)
     }
 
-    private fun createMarker(p: GeoPoint, title: String?, desc: String?, iconID: Int): Marker? {
+    private fun longPressOnMap(p: GeoPoint) {
+
+        //longPressedMarker?.let { binding.map.overlays.remove(it) }
+
+        longPressedMarker = createMarker(p, null, R.drawable.baseline_push_pin_24)
+        longPressedMarker?.let { binding.map.overlays.add(it) }
+        binding.map.invalidate()
+    }
+
+
+
+    private fun createMarker(p: GeoPoint, desc: String?, iconID: Int): Marker? {
+        val geocoder = Geocoder(this)
+        var NombreLugar= " "
+        val direccioes = geocoder.getFromLocation(p.latitude,p.longitude,1)
+        if (direccioes != null) {
+            if(direccioes.isNotEmpty()){
+                val direccion = direccioes[0]
+                NombreLugar = direccion.getAddressLine(0)
+            }
+            else{
+                NombreLugar = "No se encontro el nombre";
+            }
+        }
         var marker: Marker? = null
         if (binding.map != null) {
             marker = Marker(binding.map)
-            title?.let { marker.title = it }
-            desc?.let { marker.subDescription = it }
+            marker.title = NombreLugar
+            marker.snippet = "Lat: ${p.latitude}, Lon: ${p.longitude}"
             if (iconID != 0) {
                 val myIcon = resources.getDrawable(iconID, theme)
                 marker.icon = myIcon
             }
             marker.position = p
             marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+            marker.showInfoWindow()
         }
         return marker
     }
